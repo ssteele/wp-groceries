36dc3bef7372d5c55527f5e6daf564be355f945b
Steve Steele <steven.harville.steele@gmail.com>
2017-02-20 14:44:30 -0600

    Reorganize CurrentListTest

--- tests/CurrentListTest.php
+++ tests/CurrentListTest.php
@@ -18,35 +18,9 @@ class CurrentListTest extends \PHPUnit_Framework_TestCase
         WP_Mock::tearDown();
     }
 
-    public function testRenderExistingAdminList()
+    public function testExtractSaveGroceries()
     {
-        $userId = 1;
-        $termId = 107;
-
-        // create items
-        $groceries = [
-            [
-                'i' => $termId,
-                'a' => 0,
-                'u' => 0,
-                't' => 'i',
-                'o' => '',
-            ],
-        ];
-
-        $term = new \stdClass();
-        $term->name = 'apple';
-        WP_Mock::wpFunction('get_term_by', [
-            'args' => ['id', $termId, 'ingredient'],
-            'times' => 1,
-            'return' => $term,
-        ]);
-
-        $currentList = new CurrentList($userId);
-
-        $expected = '<li><input type="checkbox" name="i[]" id="' . $termId . '" value="' . $termId . '" /><label for="' . $termId . '"> apple</label></li>';
-        $markup = $currentList->renderExistingAdminList($groceries);
-        $this->assertEquals($expected, $markup);
+        // pass
     }
 
     public function testSaveGroceriesCurrentItemsOnly()
@@ -348,5 +322,36 @@ class CurrentListTest extends \PHPUnit_Framework_TestCase
         $currentList = new CurrentList($userId);
         $isNewIngredient = $currentList->saveGroceries($post, $sanitizerStub);
         $this->assertFalse($isNewIngredient);
+    }
+
+    public function testRenderExistingAdminList()
+    {
+        $userId = 1;
+        $termId = 107;
+
+        // create items
+        $groceries = [
+            [
+                'i' => $termId,
+                'a' => 0,
+                'u' => 0,
+                't' => 'i',
+                'o' => '',
+            ],
+        ];
+
+        $term = new \stdClass();
+        $term->name = 'apple';
+        WP_Mock::wpFunction('get_term_by', [
+            'args' => ['id', $termId, 'ingredient'],
+            'times' => 1,
+            'return' => $term,
+        ]);
+
+        $currentList = new CurrentList($userId);
+
+        $expected = '<li><input type="checkbox" name="i[]" id="' . $termId . '" value="' . $termId . '" /><label for="' . $termId . '"> apple</label></li>';
+        $markup = $currentList->renderExistingAdminList($groceries);
+        $this->assertEquals($expected, $markup);
     }
 }
