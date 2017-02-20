744e3dbcd788f99d5a1ad82d1109a8af069fc79c
Steve Steele <steven.harville.steele@gmail.com>
2017-02-19 22:26:22 -0600

    Add unit test for GroceryStore

--- tests/GroceryStoreTest.php
+++ tests/GroceryStoreTest.php
@@ -56,4 +56,49 @@ class GroceryStoreTest extends \PHPUnit_Framework_TestCase
         $returned = $groceryStore->isExistingStore($stores, $storeId);
         $this->assertEquals($expected, $returned);
     }
+
+    public function testGetFirstStore()
+    {
+        // setup
+        $userId = 1;
+        $groceryStore = new GroceryStore($userId);
+
+        $stores = [
+            (object) [
+                'id' => '16',
+                'user_id' => '1',
+                'name' => 'Lorem Ipsum',
+                'number' => '90210',
+                'street' => '123 Blah Blah',
+                'city' => 'Austin',
+                'state' => 'TX',
+                'zip' => '78753',
+            ],
+            (object) [
+                'id' => '17',
+                'user_id' => '1',
+                'name' => 'Dolor',
+                'number' => '19293',
+                'street' => '456 Bip Bop',
+                'city' => 'Austin',
+                'state' => 'TX',
+                'zip' => '78753',
+            ],
+            (object) [
+                'id' => '22',
+                'user_id' => '1',
+                'name' => 'TEST STORE',
+                'number' => '99999',
+                'street' => 'Blah',
+                'city' => 'Blah',
+                'state' => 'TX',
+                'zip' => '99999',
+            ],
+        ];
+
+        // expected first store is returned
+        $expected = 16;
+        $returned = $groceryStore->getFirstStore($stores);
+        $this->assertEquals($expected, $returned);
+    }
 }
